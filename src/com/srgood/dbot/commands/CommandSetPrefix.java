149517ae3d45aba9ffff493dcc9b121bfba88c70
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-07-20 21:21:44 -0400

    Fix CommandSetPrefix

--- src/com/srgood/dbot/commands/CommandSetPrefix.java
+++ src/com/srgood/dbot/commands/CommandSetPrefix.java
@@ -1,5 +1,7 @@
 package com.srgood.dbot.commands;
 
+import org.w3c.dom.Element;
+
 import com.srgood.dbot.BotMain;
 
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
@@ -17,13 +19,20 @@ public class CommandSetPrefix implements Command{
 	public void action(String[] args, MessageReceivedEvent event) {
 		// TODO Auto-generated method stub
 		try {
-			BotMain.servers.get(event.getGuild().getId()).put("prefix",args[0].toString());
+			Element serverNode = (Element) BotMain.servers.get(event.getGuild().getId());
+			Element prefixNode = (Element) serverNode.getElementsByTagName("prefix").item(0);
+			System.out.println("prefixNode type: " + prefixNode.getTagName());
+			System.out.println("prefixNode.getTextContent(): " + prefixNode.getTextContent());
 			event.getChannel().sendMessage("Prefix set to: " + args[0].toString());
-		} catch (Exception e){
+			prefixNode.setTextContent(args[0]);
+			System.out.println("prefixNode type: " + prefixNode.getTagName());
+			System.out.println("prefixNode.getTextContent(): " + prefixNode.getTextContent());
+		} catch (Exception e) {
 			event.getChannel().sendMessage(help().toString());
+			e.printStackTrace();
 		}
-		
-		}
+
+	}
 
 	@Override
 	public String help() {
