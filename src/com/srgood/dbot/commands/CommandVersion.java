09b37772dbb2e8ca66d3aa62aa71a15bdf289ec9
DerekGoodnow <dmanlee10@gmail.com>
2016-07-22 23:56:57 -0700

    I lied, Now I'm going to bed

---
+++ src/com/srgood/dbot/commands/CommandVersion.java
@@ -0,0 +1,31 @@
+package com.srgood.dbot.commands;
+
+import net.dv8tion.jda.events.message.MessageReceivedEvent;
+
+public class CommandVersion implements Command {
+
+	@Override
+	public boolean called(String[] args, MessageReceivedEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action(String[] args, MessageReceivedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String help() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void executed(boolean success, MessageReceivedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
