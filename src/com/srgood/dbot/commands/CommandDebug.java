f4030b7d24c12f014a4f31dfe141d5be3e1b7044
DerekGoodnow <dmanlee10@gmail.com>
2016-07-23 14:08:52 -0700

    Test

--- src/com/srgood/dbot/commands/CommandDebug.java
+++ src/com/srgood/dbot/commands/CommandDebug.java
@@ -1,8 +1,13 @@
 package com.srgood.dbot.commands;
 
+import java.awt.Color;
+
 import com.srgood.dbot.BotMain;
 
+import net.dv8tion.jda.entities.Role;
 import net.dv8tion.jda.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.exceptions.PermissionException;
+import net.dv8tion.jda.managers.RoleManager;
 
 public class CommandDebug implements Command {
 	private final String help = "Used internally for debugging. Use: '" + BotMain.prefix + "debug [debug arg]'";
@@ -34,6 +39,18 @@ public class CommandDebug implements Command {
 			"Picture url: " + event.getAuthor().getAvatarUrl().toString() + "\n" + 
 			"BotMain.jda.getSelfInfo().getAsMention().length()" + BotMain.jda.getSelfInfo().getAsMention().toString().length() + "\n" + 
 			"BotMain.jda.getSelfInfo().getAsMention()" + BotMain.jda.getSelfInfo().getAsMention());
+			try {
+				RoleManager role = event.getGuild().
+				role.setName("Reasons Admin");
+				role.setColor(Color.GREEN);
+				
+			} catch(PermissionException e3) {
+				event.getChannel().sendMessage("\n\n\n you done messed up\n" + e.getMessage());
+			}
+
+			
+			
+			
 		}
 		
 	}
