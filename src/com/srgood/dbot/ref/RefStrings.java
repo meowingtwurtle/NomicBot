ac7735f5aaea74ba37ca82847c216aaf3260f041
DerekGoodnow <dmanlee10@gmail.com>
2016-07-23 19:57:41 -0700

    over 9,000 efficiency

--- src/com/srgood/dbot/ref/RefStrings.java
+++ src/com/srgood/dbot/ref/RefStrings.java
@@ -4,6 +4,8 @@ public class RefStrings {
 	public static final String TABLE_FLIP = "(\u256F\u00B0\u25A1\u00B0\uFF09\u256F\uFE35 \u253B\u2501\u253B";
 	public static final String TABLE_UNFLIP = "\u252C\u2500\u252C\uFEFF \u30CE( \u309C-\u309C\u30CE)";
 	public static final String TABLE_UNFLIP_JOKE = TABLE_UNFLIP + " \n\n Calm down!";
+	public static final String BOT_TOKEN_SRGOOD = "MjA1NDY1MDY3ODI5OTg1Mjgx.CnW_Jw.9vZCMGk548JQ06Wpj8nLe5tfqWo";
+	public static final String BOT_TOKEN_REASONS = "MjAxODEwODM4MDcwMzYyMTEy.CnW-4w.-U2KZFTgfzXFd1HNoC_yYjF426s";
 	//see http://stackoverflow.com/questions/396429/how-do-you-know-what-version-number-to-use
 	public static final String VERSION = "0.74";
 }
