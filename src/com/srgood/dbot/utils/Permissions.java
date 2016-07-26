85fe7f96da6f2c5925f53de72e47b9439aaafd94
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-07-25 20:44:21 -0400

    Cleanup Permissions Add "STANDARD" to Permissions; Acts as "no additional permissions" Remove MEOW_MEOW_CAT

--- src/com/srgood/dbot/utils/Permissions.java
+++ src/com/srgood/dbot/utils/Permissions.java
@@ -1,7 +1,7 @@
 package com.srgood.dbot.utils;
 
 public enum Permissions {
-    DEVELOPER(99), ADMINISTRATOR(3), MEOW_MEOW_CAT(-87);
+    DEVELOPER(99), ADMINISTRATOR(3), STANDARD(0);
 
     int level;
 
