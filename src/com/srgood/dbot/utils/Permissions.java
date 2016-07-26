e4478bdcce1eb6a1f432b8efe4962296ecfffb9b
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-07-25 20:48:10 -0400

    Add xmlName and readableName to Permissions

--- src/com/srgood/dbot/utils/Permissions.java
+++ src/com/srgood/dbot/utils/Permissions.java
@@ -4,10 +4,19 @@ public enum Permissions {
     DEVELOPER(99), ADMINISTRATOR(3), STANDARD(0);
 
     int level;
-
+    String xmlName;
+    String readableName;
+    
     Permissions(int lvl) {
-        level = lvl;
+        this(lvl, "", "");
     }
+
+    Permissions(int lvl, String xmlName, String readableName) {
+        level = lvl;
+        this.xmlName = xmlName;
+        this.readableName = readableName;
+    }
+    
 
     int getLevel() {
         return level;
