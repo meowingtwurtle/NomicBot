e8e5d1f341e937a8d285fa0e82b2c8a2f4edb4cf
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-08-28 12:24:08 -0400

    Finish command threading Maybe?

--- src/com/srgood/dbot/threading/ChannelThread.java
+++ src/com/srgood/dbot/threading/ChannelThread.java
@@ -1,6 +1,8 @@
 package com.srgood.dbot.threading;
 
+import com.srgood.dbot.BotMain;
 import com.srgood.dbot.commands.CommandParser;
+import com.srgood.dbot.utils.CommandUtils;
 import net.dv8tion.jda.entities.Channel;
 
 import java.util.ArrayDeque;
@@ -10,7 +12,9 @@ public class ChannelThread extends Thread {
 
     private Channel channel;
 
-    private final Deque<CommandParser.CommandContainer> commandDeque = new ArrayDeque<>();
+    private final Deque<CommandItem> commandDeque = new ArrayDeque<>();
+
+    private boolean commandWasAdded = false;
 
     public ChannelThread(Channel channel) {
         this.channel = channel;
@@ -18,10 +22,54 @@ public class ChannelThread extends Thread {
 
     @Override
     public void run() {
+        while (true) {
+            //if the commands get method returns true (see command.class)...
+            for (int i = 0; i < commandDeque.size(); i++) {
+                CommandItem commandItem = commandDeque.getFirst();
+                CommandParser.CommandContainer commandContainer = commandItem.getCommandContainer();
+                if (commandItem.shouldExecute()) {
+                    //then run the command and its post execution code (see command)
+                    BotMain.commands.get(commandContainer.invoke).action(commandContainer.args, commandContainer.event);
+                    BotMain.commands.get(commandContainer.invoke).executed(true, commandContainer.event);
+                } else {
+                    //else only run the execution code
+                    BotMain.commands.get(commandContainer.invoke).executed(false, commandContainer.event);
+                }
+                commandDeque.removeFirst();
+            }
+            if (!commandWasAdded) { return; }
+        }
     }
 
-    public void addCommand(CommandParser.CommandContainer commandContainer) {
-        commandDeque.addLast(commandContainer);
+    private void endOfLife() {
+        CommandUtils.deregisterThread(this);
+    }
+
+    public void addCommand(CommandItem commandItem) {
+        commandDeque.addLast(commandItem);
+        commandWasAdded = true;
+    }
+
+    public static class CommandItem {
+        private final CommandParser.CommandContainer commandContainer;
+        private final boolean shouldExecute;
+
+        public CommandItem(CommandParser.CommandContainer commandContainer, boolean shouldExecute) {
+            this.commandContainer = commandContainer;
+            this.shouldExecute = shouldExecute;
+        }
+
+        public CommandParser.CommandContainer getCommandContainer() {
+            return commandContainer;
+        }
+
+        public boolean shouldExecute() {
+            return shouldExecute;
+        }
+    }
+
+    public String getChannelID() {
+        return channel.getId();
     }
 
 }
--- src/com/srgood/dbot/utils/CommandUtils.java
+++ src/com/srgood/dbot/utils/CommandUtils.java
@@ -1,31 +1,46 @@
 package com.srgood.dbot.utils;
 
+import com.srgood.dbot.BotMain;
+import com.srgood.dbot.commands.CommandParser;
+import com.srgood.dbot.threading.ChannelThread;
+
 public class CommandUtils {
-    public static void handleCommand(com.srgood.dbot.commands.CommandParser.CommandContainer cmd) {
+
+    private static java.util.Map<String, ChannelThread> channelThreadMap = new java.util.HashMap<>();
+
+    public static void handleCommand(CommandParser.CommandContainer cmd) {
         // checks if the referenced command is in the command list
-        if (com.srgood.dbot.BotMain.commands.containsKey(cmd.invoke)) {
+        if (BotMain.commands.containsKey(cmd.invoke)) {
             XMLUtils.initCommandIfNotExists(cmd);
             //if the command is enabled for the message's guild...
-            if (XMLUtils.commandIsEnabled(cmd.event.getGuild(), com.srgood.dbot.BotMain.commands.get(cmd.invoke))) {
-                //if the message author has the required permission level...
-                if (PermissionUtils.userPermissionLevel(cmd.event.getGuild(), cmd.event.getAuthor())
-                        .getLevel() >= com.srgood.dbot.BotMain.commands.get(cmd.invoke).permissionLevel(cmd.event.getGuild()).getLevel()) {
-                    boolean safe = com.srgood.dbot.BotMain.commands.get(cmd.invoke).called(cmd.args, cmd.event);
-                    //if the commands get method returns true (see command.class)...
-                    if (safe) {
-                        //then run the command and its post execution code (see command)
-                        com.srgood.dbot.BotMain.commands.get(cmd.invoke).action(cmd.args, cmd.event);
-                        com.srgood.dbot.BotMain.commands.get(cmd.invoke).executed(true, cmd.event);
+            //if the message author has the required permission level...
+            if (XMLUtils.commandIsEnabled(cmd.event.getGuild(), BotMain.commands.get(cmd.invoke)))
+                if (PermissionUtils.userPermissionLevel(cmd.event.getGuild(), cmd.event.getAuthor()).getLevel() >= BotMain.commands.get(cmd.invoke).permissionLevel(cmd.event.getGuild()).getLevel()) {
+                    boolean safe = BotMain.commands.get(cmd.invoke).called(cmd.args, cmd.event);
+                    if (channelThreadMap.containsKey(cmd.event.getChannel().getId())) {
+                        ChannelThread channelThread = channelThreadMap.get(cmd.event.getChannel().getId());
+                        channelThread.addCommand(new ChannelThread.CommandItem(cmd, safe));
+                        if (!channelThread.isAlive()) {
+                            channelThread.start();
+                        }
                     } else {
-                        //else only run the execution code
-                        com.srgood.dbot.BotMain.commands.get(cmd.invoke).executed(false, cmd.event);
+                        ChannelThread channelThread = new ChannelThread(cmd.event.getChannel());
+                        channelThread.addCommand(new ChannelThread.CommandItem(cmd, safe));
+                        channelThreadMap.put(cmd.event.getChannel().getId(), channelThread);
+                        channelThread.start();
                     }
                 } else {
                     cmd.event.getChannel().sendMessage("You lack the required permission to preform this action");
                 }
-            } else {
+            else {
                 cmd.event.getChannel().sendMessage("This command is disabled");
             }
+        }
+    }
+
+    public static void deregisterThread(ChannelThread thread) {
+        if (channelThreadMap.containsKey(thread.getChannelID())) {
+            channelThreadMap.remove(thread.getChannelID());
         }
     }
 }
