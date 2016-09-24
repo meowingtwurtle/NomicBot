aa3bf38cce9d46eb7a0469b9a5017e655fcdf39b
MeowingTwurtle <jason.e.cobb@gmail.com>
2016-09-24 13:14:45 -0400

    Add command getpermlevel

---
+++ src/com/srgood/dbot/commands/CommandGetPermLevel.java
@@ -0,0 +1,51 @@
+package com.srgood.dbot.commands;
+
+import com.srgood.dbot.PermissionLevels;
+import com.srgood.dbot.utils.CommandUtils;
+import com.srgood.dbot.utils.ConfigUtils;
+import net.dv8tion.jda.entities.Guild;
+import net.dv8tion.jda.events.message.guild.GuildMessageReceivedEvent;
+
+public class CommandGetPermLevel implements Command {
+
+    @Override
+    public boolean called(String[] args, GuildMessageReceivedEvent event) {
+        return true;
+    }
+
+    @Override
+    public void action(String[] args, GuildMessageReceivedEvent event) {
+        if (args.length < 1) {
+            event.getChannel().sendMessage("getpermlevel requires 1 argument.");
+            return;
+        }
+        String primaryAlias = CommandUtils.getPrimaryCommandAlias(args[0]);
+        Command command = CommandUtils.getCommandByName(primaryAlias);
+        event.getChannel().sendMessage(String.format("Permission level for command `%s`: **%s**", primaryAlias, ConfigUtils.getCommandPermissionXML(event.getGuild(), command).getReadableName()));
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+
+    @Override
+    public void executed(boolean success, GuildMessageReceivedEvent event) {
+
+    }
+
+    @Override
+    public PermissionLevels permissionLevel(Guild guild) {
+        return ConfigUtils.getCommandPermissionXML(guild, this);
+    }
+
+    @Override
+    public PermissionLevels defaultPermissionLevel() {
+        return PermissionLevels.STANDARD;
+    }
+
+    @Override
+    public String[] names() {
+        return new String[] {"getpermlevel"};
+    }
+}
