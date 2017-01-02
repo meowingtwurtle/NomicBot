b528a8f225b1bd4623e7111613e3dc5ec35f903d
Jason Cobb <jason.e.cobb@gmail.com>
2017-01-01 22:07:18 -0500

    Add PermissionSet class

---
+++ src/main/java/com/srgood/reasons/permissions/PermissionSet.java
@@ -0,0 +1,93 @@
+package com.srgood.reasons.permissions;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.srgood.reasons.permissions.PermissionSet.PermissionAction.ALLOW;
+import static com.srgood.reasons.permissions.PermissionSet.PermissionAction.DEFER;
+import static com.srgood.reasons.permissions.PermissionSet.PermissionAction.DENY;
+
+public class PermissionSet implements Serializable {
+
+    public PermissionSet() {
+        populatePermissionMap();
+    }
+
+    private Map<PermissibleAction, PermissionAction> permissionMap = new HashMap<>();
+
+    public PermissionSet(Collection<PermissibleAction> allowedPerms) {
+        this(allowedPerms, null, null);
+    }
+
+    public PermissionSet(Collection<PermissibleAction> allowedPerms, Collection<PermissibleAction> deniedPerms) {
+        this(allowedPerms, deniedPerms, null);
+    }
+
+    public PermissionSet(Collection<PermissibleAction> allowedPerms, Collection<PermissibleAction> deniedPerms, Collection<PermissibleAction> deferredPerms) {
+        this();
+
+        if (allowedPerms != null) {
+            for (PermissibleAction action : allowedPerms) {
+                permissionMap.put(action, ALLOW);
+            }
+        }
+
+        if (deniedPerms != null) {
+            for (PermissibleAction action : deniedPerms) {
+                permissionMap.put(action, DENY);
+            }
+        }
+
+        if (deferredPerms != null) {
+            for (PermissibleAction action : deferredPerms) {
+                permissionMap.put(action, DEFER);
+            }
+        }
+    }
+
+    private void populatePermissionMap() {
+        for (PermissibleAction action : PermissibleAction.values()) {
+            if (!permissionMap.containsKey(action)) {
+                permissionMap.put(action, DEFER);
+            }
+        }
+    }
+
+
+    public PermissionSet(PermissionSet setToDuplicate) {
+        permissionMap = new HashMap<>(new HashMap<>(setToDuplicate.permissionMap));
+    }
+
+
+    public Set<PermissibleAction> getAllowedActions() {
+        return permissionMap.entrySet()
+                            .stream()
+                            .filter(entry -> entry.getValue() == ALLOW)
+                            .map(Map.Entry::getKey)
+                            .collect(Collectors.toSet());
+    }
+
+    public Set<PermissibleAction> getDeniedActions() {
+        return permissionMap.entrySet()
+                            .stream()
+                            .filter(entry -> entry.getValue() == DENY)
+                            .map(Map.Entry::getKey)
+                            .collect(Collectors.toSet());
+    }
+
+    public Set<PermissibleAction> getDefferedActions() {
+        return permissionMap.entrySet()
+                            .stream()
+                            .filter(entry -> entry.getValue() == DEFER)
+                            .map(Map.Entry::getKey)
+                            .collect(Collectors.toSet());
+    }
+
+    public enum PermissionAction {
+        ALLOW, DEFER, DENY
+    }
+}
