27da641af4b8fd38c2f7b368336fbfec06f3203c
Jason Cobb <jason.e.cobb@gmail.com>
2017-01-12 21:26:58 -0500

    Fully remove JavaFX

--- src/main/java/com/srgood/reasons/ReasonsMain.java
+++ src/main/java/com/srgood/reasons/ReasonsMain.java
@@ -1,17 +1,10 @@
 package com.srgood.reasons;
 
-import com.srgood.reasons.app.Controller;
 import com.srgood.reasons.commands.Command;
 import com.srgood.reasons.commands.CommandParser;
 import com.srgood.reasons.config.ConfigUtils;
 import com.srgood.reasons.utils.CommandUtils;
 import com.srgood.reasons.utils.SecureOverrideKeyGenerator;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -26,11 +19,10 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.time.Instant;
 import java.util.Arrays;
 
-public class ReasonsMain extends Application {
+public class ReasonsMain {
 
     public static final Instant START_INSTANT = Instant.now();
     public static final CommandParser COMMAND_PARSER = new CommandParser();
@@ -39,7 +31,6 @@ public class ReasonsMain extends Application {
     public static String prefix;
     public static String overrideKey = SecureOverrideKeyGenerator.nextOverrideKey();
     public static ByteArrayOutputStream out, errOut;
-    private static Controller controller = null;
 
     private boolean firstTime;
     private TrayIcon trayIcon;
@@ -49,53 +40,15 @@ public class ReasonsMain extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+        new ReasonsMain().init();
     }
 
-    @Override
     public void init() {
-        initSystemOutputStreams();
         initJDA();
         initConfig();
         initCommands();
-    }
 
-    private void initSystemOutputStreams() {
-        out = new ByteArrayOutputStream();
-        errOut = new ByteArrayOutputStream();
-
-        PrintStream outPS = new PrintStream(out) {
-            @Override
-            public void println(Object o) {
-                super.println(o);
-                if (controller != null) controller.updateConsole();
-            }
-
-            @Override
-            public void println(String s) {
-                super.println(s);
-                if (controller != null) controller.updateConsole();
-            }
-        };
-
-        PrintStream errOutPS = new PrintStream(errOut) {
-            @Override
-            public void println(Object o) {
-                super.println(o);
-                if (controller != null) controller.updateErrConsole();
-            }
-
-            @Override
-            public void println(String s) {
-                super.println(s);
-                if (controller != null) controller.updateErrConsole();
-            }
-
-        };
-
-
-        System.setOut(outPS);
-        System.setErr(errOutPS);
+        addToTray();
     }
 
     private void initJDA() {
@@ -145,27 +98,7 @@ public class ReasonsMain extends Application {
         }
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new javafx.fxml.FXMLLoader(getClass().getResource("/app.fxml"));
-
-        Parent root = loader.load();
-
-        controller = loader.getController();
-
-        controller.updateConsole();
-
-        primaryStage.setTitle("Reasons Console");
-        primaryStage.setScene(new Scene(root));
-
-        addToTray(primaryStage);
-        firstTime = true;
-        Platform.setImplicitExit(false);
-
-        primaryStage.show();
-    }
-
-    public void addToTray(Stage stage) {
+    public void addToTray() {
         if (SystemTray.isSupported()) {
             SystemTray systemTray = SystemTray.getSystemTray();
             //load image here
@@ -176,26 +109,17 @@ public class ReasonsMain extends Application {
                 e.printStackTrace();
             }
 
-            stage.setOnCloseRequest(event -> hide(stage));
-
             final ActionListener closeListener = e -> {
                 System.exit(0);
             };
 
-            final ActionListener showListener = e -> Platform.runLater(stage::show);
-
             PopupMenu popup = new PopupMenu();
-
-            MenuItem showItem = new MenuItem("Show");
-            showItem.addActionListener(showListener);
-            popup.add(showItem);
 
             MenuItem closeItem = new MenuItem("Close");
             closeItem.addActionListener(closeListener);
             popup.add(closeItem);
 
             trayIcon = new TrayIcon(image, "Reasons", popup);
-            trayIcon.addActionListener(showListener);
 
             try {
                 systemTray.add(trayIcon);
@@ -203,24 +127,6 @@ public class ReasonsMain extends Application {
             } catch (AWTException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    private void hide(final Stage stage) {
-        Platform.runLater(() -> {
-            if (SystemTray.isSupported()) {
-                stage.hide();
-                showPrgmIsMinimizedMsg();
-            } else {
-                System.exit(0);
-            }
-        });
-    }
-
-    public void showPrgmIsMinimizedMsg() {
-        if (firstTime) {
-            trayIcon.displayMessage("Alert", "Reasons has been minimised to tray", TrayIcon.MessageType.INFO);
-            firstTime = false;
         }
     }
 
