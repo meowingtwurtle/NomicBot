4e2dcfcaac1f1218bcb6a9fc72236bea4c9a70e4
Jason Cobb <jason.e.cobb@gmail.com>
2017-02-05 22:28:10 -0500

    Remove now-obsolete token Reference fields

--- src/main/java/com/srgood/reasons/Reference.java
+++ src/main/java/com/srgood/reasons/Reference.java
@@ -10,9 +10,6 @@ public class Reference {
         private static final String TABLE_UNFLIP = "\u252C\u2500\u252C\uFEFF \u30CE( \u309C-\u309C\u30CE)";
         public static final String TABLE_UNFLIP_JOKE = TABLE_UNFLIP + " \n\n Calm down!";
 
-        public static final String BOT_TOKEN_REASONS = "MjAxODEwODM4MDcwMzYyMTEy.CnW-4w.-U2KZFTgfzXFd1HNoC_yYjF426s";
-        public static final String BOT_TOKEN_REASONS_DEV_1 = "MjA3MjIyNzY5MjA3NzM4MzY4.Cnf-vg.UNECSqJIHUX_9sXL-98W27XUlIU";
-        public static final String BOT_TOKEN_REASONS_DEV_2 = "MjA3MjIzMTU4OTAwNjU0MDgw.Cnf_Gw.HKBXiCldwx57cN_1uHt6XhyNEwY";
         public static final Color[] COLORS = {new Color(58, 96, 110),new Color(0, 166, 166),new Color(85, 214, 190),new Color(117, 221, 221),new Color(192, 245, 250)};
         public static final String[] EIGHT_BALL = {"It is certain","It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Dount count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"};
         //see http://stackoverflow.com/questions/396429/how-do-you-know-what-version-number-to-use
